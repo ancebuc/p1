b5cef3246deffca1a1bc496441932cdbf3b7b35c
ancebuc <ancebuc@unirioja.es>
2020-10-08 11:10:14 +0200

    1

--- Circunferencia.cpp
+++ Circunferencia.cpp
@@ -2,4 +2,31 @@
 // Created by ancebuc on 08/10/2020.
 //
 
- #include "cmake-build-debug/Circunferencia.h"
+ #include "Circunferencia.h"
+ #include cmath;
+
+Circunferencia();
+Circunferencia(double r);
+double Circunferencia::getRadio(){}
+;
+void Circunferencia::setRadio(double r){
+
+};
+double Circunferencia::getDiametro(){
+
+};
+void Circunferencia::setDiametro(double d){
+
+};
+double Circunferencia::getLongitud(){
+
+};
+void Circunferencia::setLongitud(double l){
+
+};
+double Circunferencia::getArea(){
+
+};
+void Circunferencia::setArea(double a){
+    
+};
