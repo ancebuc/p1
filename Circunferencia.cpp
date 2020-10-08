ddf1911774ec3ff15aff4836c5ac0cbc97ef29c0
ancebuc <ancebuc@unirioja.es>
2020-10-08 11:36:17 +0200

    Circunferencia

--- Circunferencia.cpp
+++ Circunferencia.cpp
@@ -3,30 +3,35 @@
 //
 
  #include "Circunferencia.h"
- #include cmath;
+ #include <cmath>;
 
-Circunferencia();
-Circunferencia(double r);
-double Circunferencia::getRadio(){}
-;
+Circunferencia::Circunferencia(){
+    Circunferencia::radio=0.0;
+};
+Circunferencia::Circunferencia(double r){
+    Circunferencia::radio=r;
+};
+double Circunferencia::getRadio(){
+    return (this->radio);
+};
 void Circunferencia::setRadio(double r){
-
+    this->radio=r;
 };
 double Circunferencia::getDiametro(){
-
+    return (getRadio()*2);
 };
 void Circunferencia::setDiametro(double d){
-
+    this->radio=d/2;
 };
 double Circunferencia::getLongitud(){
-
+    return (PI*getDiametro());
 };
 void Circunferencia::setLongitud(double l){
 
 };
 double Circunferencia::getArea(){
-
+    return (PI*pow(getRadio(),2));
 };
 void Circunferencia::setArea(double a){
-    
+
 };
