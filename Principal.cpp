a8cd912adc5e1161b93026566457113cd673b440
ancebuc <ancebuc@unirioja.es>
2020-10-14 21:13:43 +0200

    :)

--- Principal.cpp
+++ Principal.cpp
@@ -3,7 +3,7 @@
     Nombre:
     Grupo:
     Nombre del fichero: Principal_prac01_01.cpp
-    Descripción: cliente de la clase Circunferencia;
+    Descripcion: cliente de la clase Circunferencia;
 */
 
 #include <iostream>
@@ -13,30 +13,35 @@ using namespace std;
 
 int main (){
 
-    // En las siguientes líneas de código declaramos dos objetos de la clase Circunferencia;
+    // En las siguientes lï¿½neas de cï¿½digo declaramos dos objetos de la clase Circunferencia;
     // Cada uno de ellos es construido por medio de uno de los constructores propios de la clase
 
-    // Cuidado con la declaración de Circunferencia c1; o Circunferencia c1 ();
-    // La segunda versión declararía una función, no un objeto;
+    // Cuidado con la declaraciï¿½n de Circunferencia c1; o Circunferencia c1 ();
+    // La segunda versiï¿½n declararï¿½a una funciï¿½n, no un objeto;
 
     Circunferencia c1;
     Circunferencia c2 (7.5);
 
-    // Las siguientes líneas de código declaran dos punteros a objetos de la clase Circunferencia;
+    // Las siguientes lï¿½neas de cï¿½digo declaran dos punteros a objetos de la clase Circunferencia;
     // De nuevo cada uno hace uso de uno de los dos constructores disponibles en esta clase;
 
     Circunferencia * pc1 = new Circunferencia ();
     Circunferencia * pc2 = new Circunferencia (4.8);
 
-    cout << "El área de la circunferencia pc1 es " << pc1->getArea() << endl;
-    cout << "El área de la circunferencia pc1 es " << pc2->getArea() << endl;
+    cout << "El ï¿½rea de la circunferencia pc1 es " << pc1->getArea() << endl;
+    cout << "El ï¿½rea de la circunferencia pc2 es " << pc2->getArea() << endl;
 
-    cout << "El área de la circunferencia c1 es " << c1.getArea() << endl;
-    cout << "El área de la circunferencia c2 es " << c2.getArea() << endl;
+    cout << "El ï¿½rea de la circunferencia c1 es " << c1.getArea() << endl;
+    cout << "El ï¿½rea de la circunferencia c2 es " << c2.getArea() << endl;
 
-    // Introduce aquí tu propio código correspondiente a los ejercicios 4 y siguientes;
+    // Introduce aquï¿½ tu propio cï¿½digo correspondiente a los ejercicios 4 y siguientes;
 
-
-
+    //Primer ejercicio:
+    Circunferencia * cir1 = new Circunferencia();
+    cir1->setArea(314.16);
+    Circunferencia * cir2 = new Circunferencia(cir2->getRadio()+6);
+    Circunferencia * cir3 = new Circunferencia();
+    cir3->setLongitud(62.832);
+    Circunferencia * cir4 = new Circunferencia(cir4->getRadio()*5);
     return 0;
 }
